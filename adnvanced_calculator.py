cad3af9d31e3cd44129b84213f4ae3be3b807284
Nuur <103619888+nuurnisaa@users.noreply.github.com>
2023-04-11 23:23:16 +0800

    Add files via upload

---
+++ adnvanced_calculator.py
@@ -0,0 +1,17 @@
+num1=float(input("Enter first number: "))
+op=input("Enter you operator: ")
+num2=float(input("Enter second number: "))
+
+if op=="+":
+    print(num1+num2)
+elif op=="-":
+    print(num-num2)
+elif op=="*":
+    print(num1*num2)
+elif op=="/":
+    print(num1/num2)
+else:
+    print("Invalid")   
+    
+    
+
